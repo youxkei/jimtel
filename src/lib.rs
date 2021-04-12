00f25a35a7fc91f742b541be7563e0c55df0c08d
youxkei <youxkei@gmail.com>
2021-04-12 23:38:03 +0900

    Partially implement release

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,13 @@ struct Ports {
     input_gain: InputPort<Control>,
     output_gain: InputPort<Control>,
     limit: InputPort<Control>,
+    release: InputPort<Control>,
     infinite_sustain: InputPort<Control>,
 }
 
 #[uri("https://github.com/youxkei/jimtel")]
 struct Jimtel {
-    sample_rate_hz: f64,
+    sample_rate_hz: f32,
     current_peak: f32,
     current_coefficienet: f32,
     infinite_sustain: bool,
@@ -32,10 +33,10 @@ impl Plugin for Jimtel {
     type AudioFeatures = ();
 
     fn new(plugin_info: &PluginInfo, _features: &mut ()) -> Option<Self> {
-        let sample_rate_hz = plugin_info.sample_rate();
+        let sample_rate_hz = plugin_info.sample_rate() as f32;
 
         Some(Self {
-            sample_rate_hz: plugin_info.sample_rate(),
+            sample_rate_hz: sample_rate_hz,
             current_peak: 0.0,
             current_coefficienet: 0.0,
             infinite_sustain: false,
@@ -48,12 +49,14 @@ impl Plugin for Jimtel {
         let input_gain_db = *ports.input_gain;
         let output_gain_db = *ports.output_gain;
         let limit_dbfs = *ports.limit;
+        let release_ms = *ports.release;
 
         let input_gain = 10.0_f32.powf(input_gain_db * 0.05);
         let output_gain = 10.0_f32.powf(output_gain_db * 0.05);
         let total_gain = input_gain * output_gain;
         let limit = 10.0_f32.powf(limit_dbfs * 0.05);
         let infinite_sustain = *ports.infinite_sustain > 0.0;
+        let release_speed = (1.0 - limit) / (self.sample_rate_hz * release_ms / 1000.0) / 10.0;
 
         self.current_peak = self.current_peak.max(limit);
         self.current_coefficienet = limit / self.current_peak;
@@ -84,6 +87,14 @@ impl Plugin for Jimtel {
 
             if loudness > self.current_peak {
                 self.current_peak = loudness;
+                self.current_coefficienet = limit / self.current_peak;
+            } else if self.current_peak > limit {
+                self.current_peak -= release_speed;
+
+                if self.current_peak < limit {
+                    self.current_peak = limit
+                }
+
                 self.current_coefficienet = limit / self.current_peak;
             }
 
