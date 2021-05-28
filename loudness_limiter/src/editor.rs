b44eb10a8df5b825d68831ed68cc744f9a3dd4a2
youxkei <youxkei@gmail.com>
2021-05-28 22:36:49 +0900

    Use egui instead of iced

--- loudness_limiter/src/editor.rs
+++ loudness_limiter/src/editor.rs
@@ -2,181 +2,16 @@ use std::os::raw::c_void;
 use std::sync::Arc;
 
 use baseview::{Size, WindowOpenOptions, WindowScalePolicy};
-use iced_baseview::IcedWindow;
-use iced_baseview::{
-    executor, slider, text_input, Application, Color, Column, Command, Element, Length, Row,
-    Slider, Text, TextInput,
-};
+use egui::{CentralPanel, CtxRef, Grid, Style};
+use egui_baseview::{EguiWindow, Queue, RenderSettings, Settings};
 use raw_window_handle::{HasRawWindowHandle, RawWindowHandle};
 use vst::editor::Editor;
 
 use crate::params::LoudnessLimiterParams;
 
-#[derive(Debug, Clone)]
-pub enum Message {
-    SliderChanged(i32, f32),
-    TextChanged(i32, String),
-}
+struct ParentWindowHandle(*mut c_void);
 
-impl Message {
-    fn slider_changed(index: i32) -> Box<dyn Fn(f32) -> Message> {
-        Box::new(move |value| Message::SliderChanged(index, value))
-    }
-
-    fn text_changed(index: i32) -> Box<dyn Fn(String) -> Message> {
-        Box::new(move |value| Message::TextChanged(index, value))
-    }
-}
-
-pub struct Flags {
-    pub params: Arc<LoudnessLimiterParams>,
-}
-
-struct ParamUI {
-    params: Arc<LoudnessLimiterParams>,
-    index: i32,
-    text: Option<String>,
-
-    slider_state: slider::State,
-    text_input_state: text_input::State,
-}
-
-impl ParamUI {
-    fn new(params: Arc<LoudnessLimiterParams>, index: i32) -> Self {
-        Self {
-            params,
-            index,
-            text: None,
-            slider_state: Default::default(),
-            text_input_state: Default::default(),
-        }
-    }
-
-    fn view(&mut self) -> Row<Message> {
-        if let None = self.text {
-            self.text = Some(self.params.get_value_text(self.index))
-        }
-
-        Row::new()
-            .spacing(8)
-            .push(
-                Text::new(&self.params.get_name(self.index))
-                    .size(32)
-                    .width(Length::Units(128)),
-            )
-            .push(
-                Slider::new(
-                    &mut self.slider_state,
-                    self.params.get_range(self.index),
-                    self.params.get_value(self.index),
-                    Message::slider_changed(self.index),
-                )
-                .step(0.1)
-                .height(32),
-            )
-            .push(
-                TextInput::new(
-                    &mut self.text_input_state,
-                    "",
-                    self.text.as_ref().unwrap(),
-                    Message::text_changed(self.index),
-                )
-                .size(32)
-                .width(Length::Units(96)),
-            )
-            .push(
-                Text::new(self.params.get_unit(self.index))
-                    .size(32)
-                    .width(Length::Units(64)),
-            )
-    }
-}
-
-pub struct LoudnessLimiterUI {
-    params: Arc<LoudnessLimiterParams>,
-    param_uis: Vec<ParamUI>,
-}
-
-impl Application for LoudnessLimiterUI {
-    type Executor = executor::Default;
-    type Message = Message;
-    type Flags = Flags;
-
-    fn new(flags: Self::Flags) -> (Self, Command<Message>) {
-        let params = flags.params;
-
-        let param_uis: Vec<ParamUI> = LoudnessLimiterParams::index_range()
-            .map(|index| ParamUI::new(params.clone(), index))
-            .collect();
-
-        (
-            Self {
-                params: params.clone(),
-                param_uis,
-            },
-            Command::none(),
-        )
-    }
-
-    fn update(&mut self, message: Message) -> Command<Message> {
-        match message {
-            Message::SliderChanged(index, value) => {
-                self.params.set_value(index, value);
-                self.param_uis[index as usize].text = Some(self.params.get_value_text(index))
-            }
-
-            Message::TextChanged(index, value) => match value.parse::<f32>() {
-                Ok(parsed_value) if !value.ends_with(".") => {
-                    let range = self.params.get_range(index);
-                    let value = parsed_value.min(*range.end()).max(*range.start());
-
-                    self.params.set_value(index, value);
-                    self.param_uis[index as usize].text = Some(value.to_string());
-                }
-
-                _ => {
-                    self.param_uis[index as usize].text = Some(value);
-                }
-            },
-        }
-
-        Command::none()
-    }
-
-    fn view(&mut self) -> Element<Message> {
-        let mut column = Column::new().padding(16).spacing(16);
-
-        for param_ui in self.param_uis.iter_mut() {
-            column = column.push(param_ui.view())
-        }
-
-        let element: Element<Message> = column.into();
-
-        element
-    }
-
-    fn background_color(&self) -> Color {
-        Color::WHITE
-    }
-}
-
-pub struct LoudnessLimiterEditor {
-    handle: Option<iced_baseview::WindowHandle<Message>>,
-    params: Arc<LoudnessLimiterParams>,
-}
-
-impl LoudnessLimiterEditor {
-    pub fn new(params: Arc<LoudnessLimiterParams>) -> Self {
-        Self {
-            handle: None,
-            params,
-        }
-    }
-}
-
-struct WindowHandle(*mut c_void);
-
-unsafe impl HasRawWindowHandle for WindowHandle {
+unsafe impl HasRawWindowHandle for ParentWindowHandle {
     #[cfg(any(
         target_os = "linux",
         target_os = "dragonfly",
@@ -214,6 +49,30 @@ unsafe impl HasRawWindowHandle for WindowHandle {
     }
 }
 
+pub struct LoudnessLimiterEditor {
+    opened: bool,
+    params: Arc<LoudnessLimiterParams>,
+}
+
+impl LoudnessLimiterEditor {
+    pub fn new(params: Arc<LoudnessLimiterParams>) -> Self {
+        Self {
+            params,
+            opened: false,
+        }
+    }
+}
+
+struct State {
+    params: Arc<LoudnessLimiterParams>,
+}
+
+impl State {
+    fn new(params: Arc<LoudnessLimiterParams>) -> Self {
+        State { params }
+    }
+}
+
 impl Editor for LoudnessLimiterEditor {
     fn size(&self) -> (i32, i32) {
         (1024, 512)
@@ -224,35 +83,62 @@ impl Editor for LoudnessLimiterEditor {
     }
 
     fn open(&mut self, parent: *mut c_void) -> bool {
-        let settings = iced_baseview::Settings {
+        if self.opened {
+            return false;
+        }
+
+        let settings = Settings {
             window: WindowOpenOptions {
                 title: "Jimtel Loudness Limiter".to_string(),
                 size: Size::new(1024.0, 512.0),
-                scale: WindowScalePolicy::ScaleFactor(1.0),
+                scale: WindowScalePolicy::ScaleFactor(2.0),
             },
-            flags: Flags {
-                params: self.params.clone(),
-            },
+            render_settings: RenderSettings::default(),
         };
 
-        let handle =
-            IcedWindow::<LoudnessLimiterUI>::open_parented(&WindowHandle(parent), settings);
+        EguiWindow::open_parented(
+            &ParentWindowHandle(parent),
+            settings,
+            State::new(self.params.clone()),
+            |egui_ctx: &CtxRef, _queue: &mut Queue, _state: &mut State| {
+                let mut style: Style = (*egui_ctx.style()).clone();
+                style.spacing.slider_width *= 3.0;
+                egui_ctx.set_style(style);
+            },
+            |egui_ctx: &CtxRef, _queue: &mut Queue, state: &mut State| {
+                CentralPanel::default().show(&egui_ctx, |ui| {
+                    Grid::new("root grid").striped(true).show(ui, |ui| {
+                        for index in LoudnessLimiterParams::index_range() {
+                            let mut value = state.params.get_value(index);
 
-        self.handle = Some(handle);
+                            ui.label(state.params.get_name(index));
+
+                            if ui
+                                .add(
+                                    egui::Slider::new(&mut value, state.params.get_range(index))
+                                        .clamp_to_range(true)
+                                        .suffix(state.params.get_unit(index)),
+                                )
+                                .changed()
+                            {
+                                state.params.set_value(index, value);
+                            }
+
+                            ui.end_row();
+                        }
+                    });
+                });
+            },
+        );
 
         true
     }
 
     fn is_open(&mut self) -> bool {
-        self.handle.is_some()
+        self.opened
     }
 
     fn close(&mut self) {
-        match self.handle {
-            Some(ref mut handle) => handle.close_window().unwrap(),
-            None => {}
-        };
-
-        self.handle = None;
+        self.opened = false;
     }
 }
